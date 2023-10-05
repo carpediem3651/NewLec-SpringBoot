58caf7b692ffb6c652d10a60b583e18bb1202579
NamKiHyeok <carpediem7490@gmail.com>
2023-10-06 08:41:26 +0900

    화면수정

--- src/main/resources/static/js/menu/list.js
+++ src/main/resources/static/js/menu/list.js
@@ -1,15 +1,21 @@
-/* 클릭시 DOM 이벤트 발생*/
 window.addEventListener("load", function(){
-	/* 이벤트가 발생할 대상 변수 선언*/
-  let formSection = document.querySelector(".form-section");
-  let findButton = formSection.querySelector('.btn-find');
-  let queryInput = formSection.querySelector('.input-query')
-  
-  btnSubmit.onclick = function(e) {
-	  e.preventDefault();
-	  
-	  let query = queryInput.value;
-	  
-	  
-  }
+	//let btnSubmit = document.querySelector(".form-section input[type=submit]")
+	let fromSection = document.querySelector(".form-section")
+	let findButton = document.querySelector(".btn-find")
+	let queryInput = document.querySelector(".input-query"); // document에서 입력객체 얻기 
+	
+	findButton.onclick= function(e){
+		e.preventDefault();
+		
+		let query = queryInput.value; // 입력객체의 속성에서입력값을 
+		
+		console.log(query);
+
+
+	}
 });
+
+window.addEventListener("load", function(){
+	let categorySerch = document.querySelector("")
+});
+
